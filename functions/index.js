c5add8410e45a218d20bdcb456b9ee42f85eeecb
ZachTheDev <zach@kodebalt.com>
2024-03-23 20:24:31 -0500

    another attempt again again

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 export async function onRequestGet(context) {
   context.env.LOGGER.fetch(context.request);
   const request = context.request;
-  return new Response(request.body, request.headers);
+  return env.ASSETS.fetch(request);
 }
