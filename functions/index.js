7864cdcb50ac329db6b8db6e0c2790db3037fe70
ZachTheDev <zach@kodebalt.com>
2024-03-23 21:08:58 -0500

    Revert "Add waitUntil"
    This reverts commit b0586ad006363746432b61b7a3f6607b96d2ac94.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 export async function onRequestGet(context) {
-  context.waitUntil(context.env.LOGGER.fetch(context.request));
-  //  context.env.LOGGER.fetch(context.request);
+  context.env.LOGGER.fetch(context.request);
   return context.env.ASSETS.fetch(context.request);
 }
