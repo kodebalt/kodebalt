8d42f937e3346ca59d319b484cd355a6389c0d44
ZachTheDev <zach@kodebalt.com>
2024-04-08 19:01:37 -0500

    Fix useragent check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-if (!navigator.userAgent.includes("Googlebot")) {
+const googlebotKeywords = ["Googlebot", "Google-InspectionTool"];
+const isGooglebot = googlebotKeywords.some((keyword) => navigator.userAgent.includes(keyword));
+if (!isGooglebot) {
   document.title = "Kodebalt: Page Not Found";
   document.querySelector("#header").textContent = "Page Not Found";
   document.querySelector("#subheaderContainer > .text").textContent = "Sorry, but the page you were trying to view does not exist, yet...";
