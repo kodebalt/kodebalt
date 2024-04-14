bfad78cc740f55e0a59b802eac777b799d2f5840
ZachTheDev <zach@kodebalt.com>
2024-04-14 16:47:40 -0500

    Add bingbot

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 import { revertVisibility, resizeSubheader } from "./shared.js";
 
-const googlebotKeywords = ["Googlebot", "Google-InspectionTool"];
-const isGooglebot = googlebotKeywords.some((keyword) => navigator.userAgent.includes(keyword));
-if (!isGooglebot) {
+const botKeywords = ["Googlebot", "Google-InspectionTool", "bingbot"];
+const isBot = botKeywords.some((keyword) => navigator.userAgent.includes(keyword));
+if (!isBot) {
   document.title = "Kodebalt: Page Not Found";
   document.querySelector("#header").textContent = "Page Not Found";
   document.querySelector("#subheaderContainer > .text").textContent = "Sorry, but the page you were trying to view does not exist, yet...";
